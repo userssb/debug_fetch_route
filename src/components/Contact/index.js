c81acf1df7a4f718e101db1d70be75f73059e042
userssb <sreeramoju.sreekanthbabu@gmail.com>
2022-06-21 15:43:11 +0000

    commit message

---
+++ src/components/Contact/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const Contact = () => (
+  <div className="contact-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+      alt="contact"
+      className="contact-img"
+    />
+    <h1 className="contact-heading">Contact</h1>
+    <h3 className="contact">Nanitha Nagireddy: +91 99127 41105</h3>
+  </div>
+)
+
+export default Contact
